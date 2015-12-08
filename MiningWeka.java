820dadc73f86713afd7dae158a36e1da22121330
Eldridg <eldridg.soetanto@bobcats.gcsu.edu>
2015-12-08 09:50:21 +0100

    Update MiningWeka.java

--- MiningWeka.java
+++ MiningWeka.java
@@ -2,6 +2,7 @@ package MiningWeka;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,29 +12,58 @@ import java.util.Scanner;
 
 public class MiningWeka {
 
-public static void main(String args[]) throws IOException {	  
-			
-//con-13.con to weka-13.txt
-	        File cons = new File("C:/Users/Admin/VUwork/con-13.txt");
-	        Scanner scanner = new Scanner(cons);
-	        ArrayList<String> listArray = new ArrayList<String>();
-	        while(scanner.hasNextLine()){
-	            String line = scanner.nextLine();
-	            String end = "\"," + line.substring(line.lastIndexOf(" ")+1);
-	            line = line.replaceAll(" [^ ]+$", "");
-	            String result ="'\"" + line + end;
-	            
-	            listArray.add(result);
-	            String listString = "";
-	            
-	            for (String s : listArray){
-	            	listString += s + "\n";
-	            }
-	        
-	            String path = "C:/Users/Admin/VUwork/weka-13.txt";
-	            Files.write(Paths.get(path), listString.getBytes(), StandardOpenOption.CREATE);
-		}
-	        scanner.close();	
-}
-}
+	public static void main(String args[]) throws IOException {
 
+		// con-13.con to weka-13.txt
+		File files = new File("/Users/Eldridg/VUwork/CompleteTextfiles/");
+		File[] fileList = files.listFiles();
+		File arff = new File("/Users/Eldridg/VUwork/ONEFile/allrecords.csv");
+
+		FileWriter fileWriter = new FileWriter(arff, true);
+		fileWriter.append("word,tag,POS-tag,chunk");
+		for (int i = 0; i < fileList.length; i++) {
+			File file = fileList[i];
+
+			String weka = new Scanner(new File(
+					"/Users/Eldridg/VUwork/CompleteTextfiles/"
+							+ file.getName().substring(0,
+									file.getName().length() - 4) + ".csv"))
+					.useDelimiter("\\Z").next();
+
+			// Create new .CSV file with prefix
+			// File arff = new File("/Users/Eldridg/VUwork/NEWINPUT/" +
+			// file.getName().substring(0, file.getName().length()-4) + ".csv");
+
+			fileWriter.append("\n" + weka);
+			
+
+			/*
+			 * Scanner scanner = new Scanner(file); ArrayList<String> listArray
+			 * = new ArrayList<String>(); while (scanner.hasNextLine()) { String
+			 * line = scanner.nextLine(); //String end = "\"," +
+			 * line.substring(line.lastIndexOf(" ") + 1); //line =
+			 * line.replaceAll(" [^ ]+$", ""); //String result = "\"" + line +
+			 * end;
+			 * 
+			 * listArray.add(line); String listString = "";
+			 * 
+			 * for (String s : listArray) { listString += s + "\n"; }
+			 * 
+			 * //String path = "/Users/Eldridg/VUwork/NEWINPUT/" +
+			 * file.getName().substring(0, file.getName().length()-4) + ".csv";
+			 * //Files.write(Paths.get(path), listString.getBytes(), //
+			 * StandardOpenOption.CREATE); }
+			 * 
+			 * String weka = new Scanner(new File(
+			 * "/Users/Eldridg/VUwork/CompleteTextfiles/" +
+			 * file.getName().substring(0, file.getName().length()-4) +
+			 * ".csv")).useDelimiter("\\Z") .next(); fileWriter.append(weka);
+			 * fileWriter.close();
+			 */
+			// scanner.close();
+		} 
+		fileWriter.close();
+
+
+	}		
+}
